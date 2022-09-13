9edd18e8e7634b9e073de4a5d32836c9aefd5f3e
martirosharutyunyan <martiros.harutunyan@gmail.com>
2022-09-13 18:52:02 +0400

    added migration functional creating table and getting differences

--- mian.go
+++ mian.go
@@ -1,17 +1,75 @@
 package main
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
-	_ "github.com/uptrace/bun/driver/pgdriver"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
-func main() {
-	dsn := "postgresql://postgres:postgres@localhost:5432/learning"
+type MigrationEntity struct {
+	gorm.Model
+	name string
+}
 
-	_, err := sql.Open("pg", dsn)
+func main() {
+	content, err := os.ReadFile("./dbconfig.json")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println("Connected")
+
+	var configJson = make(map[string]any)
+	marshalErr := json.Unmarshal(content, &configJson)
+
+	if marshalErr != nil {
+		log.Fatal(marshalErr)
+	}
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", configJson["host"], configJson["user"], configJson["password"], configJson["dbname"], configJson["port"])
+
+	db, connectionErr := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if connectionErr != nil {
+		fmt.Println(connectionErr)
+	}
+
+	db.AutoMigrate(&MigrationEntity{})
+
+	fmt.Println("Connected to database")
+
+	folderMigrations, directoryErr := os.ReadDir(configJson["migrations"].(string))
+
+	if directoryErr != nil {
+		log.Fatal(directoryErr)
+	}
+	var migrationNames []string
+	for _, migration := range folderMigrations {
+		migrationNames = append(migrationNames, migration.Name())
+	}
+
+	var migrationEntities []MigrationEntity
+	db.Model(&MigrationEntity{}).Scan(migrationEntities)
+
+	fmt.Println(migrationEntities)
+
+	var migrationDifferences []string
+	for _, migrationName := range migrationNames {
+		for _, entity := range migrationEntities {
+			if migrationName != entity.name {
+				migrationDifferences = append(migrationDifferences, migrationName)
+			}
+		}
+	}
+
+	for _, migrationName := range migrationDifferences {
+		migrationSql, readFileErr := os.ReadFile(configJson["migrations"].(string) + migrationName)
+
+		if readFileErr != nil {
+			log.Fatal(readFileErr)
+		}
+
+		fmt.Println(migrationSql)
+	}
+
 }

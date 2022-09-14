83b4adafd5627253b98703dfa592e972da217bde
martirosharutyunyan <martiros.harutunyan@gmail.com>
2022-09-14 10:05:42 +0400

    divided functions and added generate logic

--- mian.go
+++ mian.go
@@ -7,6 +7,7 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"os"
+	"time"
 )
 
 type MigrationEntity struct {
@@ -27,6 +28,22 @@ func main() {
 		log.Fatal(marshalErr)
 	}
 
+	cliArgument := os.Args[1]
+
+	switch cliArgument {
+	case "--sync":
+		MigrationSync(configJson)
+		fmt.Println("Migrations synced to database")
+	case "--generate":
+		if len(os.Args) != 3 {
+			log.Fatal("Please provide the migration name")
+		}
+		GenerateMigration(os.Args[2], configJson)
+	}
+}
+
+func MigrationSync(configJson map[string]any) {
+
 	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", configJson["host"], configJson["user"], configJson["password"], configJson["dbname"], configJson["port"])
 
 	db, connectionErr := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -34,40 +51,80 @@ func main() {
 		fmt.Println(connectionErr)
 	}
 
-	db.AutoMigrate(&MigrationEntity{})
-
-	fmt.Println("Connected to database")
+	autoMigrateErr := db.AutoMigrate(&MigrationEntity{})
+	if autoMigrateErr != nil {
+		fmt.Println(autoMigrateErr)
+	}
 
 	folderMigrations, directoryErr := os.ReadDir(configJson["migrations"].(string))
 
 	if directoryErr != nil {
 		log.Fatal(directoryErr)
 	}
-	var migrationNames []string
-	for _, migration := range folderMigrations {
-		migrationNames = append(migrationNames, migration.Name())
-	}
 
 	var migrationEntities []MigrationEntity
-	db.Model(&MigrationEntity{}).Scan(migrationEntities)
+	db.Model(&MigrationEntity{}).Scan(&migrationEntities)
 
-	var migrationDifferences []string
-	for _, migrationName := range migrationNames {
-		for _, entity := range migrationEntities {
-			if migrationName != entity.Name {
-				migrationDifferences = append(migrationDifferences, migrationName)
-			}
-		}
-	}
+	notSyncedMigrations := GetNotSyncedMigrations(folderMigrations, migrationEntities)
 
-	for _, migrationName := range migrationDifferences {
-		migrationSql, readFileErr := os.ReadFile(configJson["migrations"].(string) + migrationName)
+	for _, migrationName := range notSyncedMigrations {
+		migrationSqlBuffer, readFileErr := os.ReadFile(configJson["migrations"].(string) + "/" + migrationName)
 
 		if readFileErr != nil {
 			log.Fatal(readFileErr)
 		}
 
-		fmt.Println(migrationSql)
+		migrationSql := string(migrationSqlBuffer)
+		db.Exec(migrationSql)
+		db.Create(&MigrationEntity{Name: migrationName})
+	}
+}
+
+func GenerateMigration(name string, configJson map[string]any) {
+	timestamp := time.Now().Unix()
+	file, err := os.Create(fmt.Sprintf("%v/%v-%v.sql", configJson["migrations"], timestamp, name))
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetNotSyncedMigrations(folderMigrations []os.DirEntry, migrationEntities []MigrationEntity) []string {
+	var notSyncedMigrations []string
+	for _, folderMigration := range folderMigrations {
+		existMigration := false
+
+		for _, migrationEntity := range migrationEntities {
+			if folderMigration.Name() == migrationEntity.Name {
+				existMigration = true
+			}
+		}
+
+		if !existMigration {
+			notSyncedMigrations = append(notSyncedMigrations, folderMigration.Name())
+		}
+
 	}
 
+	var deletedMigrations []string
+	for _, migrationEntity := range migrationEntities {
+		existMigration := false
+
+		for _, folderMigration := range folderMigrations {
+			if folderMigration.Name() == migrationEntity.Name {
+				existMigration = true
+			}
+		}
+
+		if !existMigration {
+			deletedMigrations = append(deletedMigrations, migrationEntity.Name)
+		}
+	}
+
+	if len(deletedMigrations) != 0 {
+		fmt.Println("From folder deleted migrations :", deletedMigrations)
+	}
+
+	return notSyncedMigrations
 }
